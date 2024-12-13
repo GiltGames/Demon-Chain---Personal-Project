3477b4c06b43ae9739841a3a8aa1827003c5ae3d
GiltGames <info@giltgames.com>
2024-12-13 15:42:00 +0000

    Week2

--- Assets/Scripts/sDemonMove.cs
+++ Assets/Scripts/sDemonMove.cs
@@ -1,3 +1,5 @@
+
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class sDemonMove : MonoBehaviour
@@ -7,6 +9,16 @@ public class sDemonMove : MonoBehaviour
     public int vDemonMoveState;
     public GameObject Hand;
     public GameObject Player;
+    public GameObject PowerMeter;
+    public GameObject DemonLight1;
+    public GameObject DemonLight2;
+    public GameObject DemonLight3;
+    public Light dL1;
+    public Light dL2;
+    public Light dL3;
+    public float vDemonLightLevel;
+    public float vDemonLightLevelMax;
+    public float vLightset;
     public float vFireSpeed;
     public float vFireSpeedMax = 100;
     public float vFireSpeedInc = 20;
@@ -16,6 +28,8 @@ public class sDemonMove : MonoBehaviour
     public Vector3 vHomeDir;
     public float vDemonReturnSpeed;
     public float vDemonHomeVariance = 0.1f;
+    public float vMouseSensitityDemon=1;
+    public float tSlowdown = 1f;
 
 
 
@@ -23,6 +37,9 @@ public class sDemonMove : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        dL1 = DemonLight1.GetComponent<Light>();
+        dL2 = DemonLight2.GetComponent<Light>();
+        dL3 = DemonLight3.GetComponent<Light>();
     }
 
     // Update is called once per frame
@@ -74,6 +91,13 @@ public class sDemonMove : MonoBehaviour
     void MouseDirInput()
 
     {
+        float vMouseX=Input.GetAxis("MouseX");
+        float vMouseY = Input.GetAxis("MouseY");
+
+        //direction change mechanism is to add vertical/horizontal force
+
+        rb.AddForce(UnityEngine.Vector3.up*vMouseY*vMouseSensitityDemon*rb.linearVelocity.magnitude,ForceMode.Impulse);
+        rb.AddForce(transform.right * vMouseX * vMouseSensitityDemon * rb.linearVelocity.magnitude, ForceMode.Impulse);
 
 
 
@@ -96,6 +120,12 @@ public class sDemonMove : MonoBehaviour
                 vFireSpeed = vFireSpeedMax;
 
             }
+
+            vDemonLightLevel = vDemonLightLevelMax * (vFireSpeed / vFireSpeedMax);
+
+            vLightset = vDemonLightLevel;
+            pSetLight();
+
         }
 
         vMouseFireLetGo = Input.GetButtonUp("Fire1");
@@ -119,7 +149,12 @@ public class sDemonMove : MonoBehaviour
         if(Input.GetAxis("Fire1")>0)
         {
             vDemonMoveState = 2;
-            rb.linearVelocity = Vector3.zero;
+            rb.linearVelocity = UnityEngine.Vector3.zero;
+            vLightset = 0;
+            pSetLight();
+
+
+
         }
 
 
@@ -143,12 +178,30 @@ public class sDemonMove : MonoBehaviour
             transform.position = Hand.transform.position;
             rb.linearVelocity = Vector3.zero;
         }
+
+
+        //reduce speed just before getting back
+      
+        if (vHomeDir.magnitude < vDemonHomeVariance*5)
+        {
+            tSlowdown =.3f;
+
+        }
         
-        transform.position = transform.position + vHomeDir.normalized * vDemonReturnSpeed *Time.deltaTime;
+        
 
+            transform.position = transform.position + vHomeDir.normalized * tSlowdown* vDemonReturnSpeed *Time.deltaTime;
 
+        tSlowdown = 1;
 
     }
 
+    void pSetLight()
+    {
+        dL1.intensity = vLightset;
+        dL2.intensity = vLightset;
+        dL3.intensity = vLightset;
+
+    }
 
 }
---
+++ Assets/Scripts/sEnemy.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class sEnemy : MonoBehaviour
+{
+    public float vHealth;
+    public float vHealthMax;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pDead()
+    {
+
+
+
+    }
+
+}
